b9e3a6bd26711addaaf7950695bf1d65471e8281
KunalHaker <kunal.bathamn@quovantis.com>
2022-06-24 11:01:34 +0530

    Added String methods

---
+++ String Class/StringContains.java
@@ -0,0 +1,7 @@
+public class StringContains {
+    public static void main(String[] args) {
+        String str="Hy I am good how are you?";
+        System.out.println(str.contains("good"));
+        System.out.println(str.contains("dwadwa"));
+    }
+}
